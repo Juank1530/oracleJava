a068ceeb127fa35c75ab42972d6cff3cdb6a07c3
juank153021@gmail.com <juank153021@gmail.com>
2023-11-16 16:56:47 -0500

    Lesson about variables

---
+++ ThirdPart/src/secondCuorse/SmileFace.java
@@ -0,0 +1,38 @@
+package secondCuorse;
+
+public class SmileFace {
+
+	public static void main(String[] args) {
+		System.out.println("  8888  ");
+		System.out.println(" 8    8 ");
+		System.out.println("8 8  8 8");
+		System.out.println("8      8");
+		System.out.println("8 8  8 8");
+		System.out.println("8  88  8");
+		System.out.println(" 8    8 ");
+		System.out.println("  8888  ");
+		
+		
+		System.out.println("……..@*@*");
+		System.out.println("….@*……..@* …………………………@*");
+		System.out.println("..@*……………@* ………………@*……..@*");
+		System.out.println(".@*……………….@*……….@*……………..@*");
+		System.out.println("@*…………………..@*…@*………………….@*");
+		System.out.println("@*………………………*……………………..@*");
+		System.out.println(".@*…………………………………………….@*");
+		System.out.println("..@*………………………………………..@*");
+		System.out.println("….@*…………………………………..@*");
+		System.out.println("……..@*…………………………..@*");
+		System.out.println("………..@*……………………@*");
+		System.out.println("…………….@*…………..@*");
+		System.out.println("……………….@*……@*");
+		System.out.println("………………….*..@*");
+		System.out.println("……………………@");
+		System.out.println("……………………*");
+		System.out.println("……………………@");
+		System.out.println("……………………*");
+				
+
+	}
+
+}
---
+++ ThirdPart/src/variables/Variables03.java
@@ -0,0 +1,35 @@
+package	variables;
+
+public class Variables03 {
+
+    public static void main(String[] args) {
+        
+        boolean bool = true;
+        
+        int intVar1 = 15;
+        int intVar2 = 2;
+        int Var3 = 3;
+    
+        double doubleVar1, doubleVar2, doubleVar3, doubleVar4;
+        double Var1 = 1.1;
+        double Var2 = 2.1;
+        double Var3_1 = 3.1;
+        double Var4 = 4.1;
+        
+    
+        String stringVar1 = "one";
+        String stringVar2 = "2";
+
+        //Don't edit these print statements
+        System.out.println("bool = "        +bool);
+        System.out.println("intVar1 = "     +intVar1);
+        System.out.println("intVar2 = "     +intVar2);
+        System.out.println("intVar3 = "     +Var3);
+        System.out.println("doubleVar1 = "  +Var1);
+        System.out.println("doubleVar2 = "  +Var2);
+        System.out.println("doubleVar3 = "  +Var3_1);
+        System.out.println("doubleVar4 = "  +Var4);
+        System.out.println("stringVar1 = "  +(stringVar1 +1));
+        System.out.println("stringVar2 = "  +(stringVar2 +2));
+    }
+}
